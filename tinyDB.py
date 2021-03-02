170056f660ef4991fca4f9abb7bc4e30df8f82dc
mikaelpo <47596730+mikaelpo@users.noreply.github.com>
2021-03-02 11:24:15 +0200

    Update tinyDB.py
    Funktio lisätty

--- tinyDB.py
+++ tinyDB.py
@@ -5,17 +5,18 @@ Created on Mon Mar  1 09:52:02 2021
 @author: mikae
 """
 
-from datetime import date
+#from datetime import date
 from tinydb import TinyDB, Query
 
-today = date.today()
-d1 = today.strftime("%d/%m/%Y")
+#today = date.today()
+#d1 = today.strftime("%d/%m/%Y")
 
-db = TinyDB('db.json')
-
-studyTime = 0
-breakLength = [1,2,3,4,5]
-breaks = len(breakLength)
-
-db.insert({"paiva": d1, "Opiskeluaika" : studyTime, "Taukojen lkm": breaks, "Taukojen pituudet": breakLength})
+def addDatabase(date, studyTime, breakLength):
+    db = TinyDB('db.json')
+    
+    #studyTime = 0
+    #breakLength = [1,2,3,4,5]
+    breaks = len(breakLength)
+    
+    db.insert({"paiva": date, "Opiskeluaika" : studyTime, "Taukojen lkm": breaks, "Taukojen pituudet": breakLength})
 
