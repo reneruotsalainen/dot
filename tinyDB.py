8cf2b2628e65febecbb51679f97840c73f414231
mikaelpo <47596730+mikaelpo@users.noreply.github.com>
2021-03-12 16:19:29 +0200

    Update tinyDB.py
    Added getAvgBreak and saveAvgBreak. getAvgBreak returns average time study time between breaks

--- tinyDB.py
+++ tinyDB.py
@@ -4,36 +4,75 @@ Created on Mon Mar  1 09:52:02 2021
 
 @author: mikae
 """
-
-
+import datetime as dt
 from tinydb import TinyDB, Query
 
-#studyTime = 0
-#breakLength = [2,2]
+#testvalues
+#now = dt.datetime.now()
+#testStart = dt.datetime(2021, 3, 12, 16, 30)
+#testStop = dt.datetime(2021, 3, 12, 16, 45)
+#testDate = now
+#testStudyTime = 5
+#[[kesto, tauonalotus, lopetus]]
+#testBreakLength = [[10,testStart,testStop],[12,testStart,testStop]]
+
 
 db = TinyDB('db.json')
 breakLengthDB = TinyDB('breakLengthDB.json')
 
-def addDatabase(date, studyTime, breakLength):
+def addDatabase(start, studyTime, breakLength):
     
+    saveAvgBreak(start, breakLength)
+    
+    convertedBreaks = []
+    for i in breakLength:
+        temp = []
+        temp.append(i[0])
+        temp.append(str(i[1]))
+        temp.append(str(i[2]))
+        convertedBreaks.append(temp)
+        
     breaks = len(breakLength)
-    db.insert({"paiva": date, "Opiskeluaika" : studyTime, "Taukojen_lkm": breaks, "Taukojen_pituudet": breakLength})
-    
-    getBreakAvg(breakLength)
+    db.insert({"paiva": str(start), "Opiskeluaika" : studyTime, "Taukojen_lkm": breaks, "Taukojen_pituudet": convertedBreaks})
     
     
-def getBreakAvg(breakLength):
     
-    avg = sum(breakLength)/len(breakLength)
-   
+def getBreakAvg():
     #if list is empty insert avg, else get avg and calculate new avg
     if not breakLengthDB:
-        breakLengthDB.insert({"Taukojen_keskiarvo" : avg})
-        return avg
+        return None
     else:
         for i in breakLengthDB:
-            tmp = i.get("Taukojen_keskiarvo")
-            tmp = (avg + tmp) / 2
-            breakLengthDB.truncate()
-            breakLengthDB.insert({"Taukojen_keskiarvo" : tmp})
-            return tmp
+            avg = i.get("Taukojen_valit")
+            return (sum(avg)/len(avg))
+
+def saveAvgBreak(start, breakLength):
+    times = []
+    times.append(breakLength[0][1]-start)
+    
+    i = 0
+    while i < len(breakLength):
+        if (i + 1 >= len(breakLength)):
+            break;
+        else:
+            times.append(breakLength[i+1][2]-breakLength[i][1])
+            i += 1
+       
+    seconds = []
+
+    for i in times:
+        seconds.append(i.total_seconds())
+          
+    if not breakLengthDB:
+        breakLengthDB.insert({"Taukojen_valit" : seconds})
+    else:
+       for i in breakLengthDB:
+           avg = i.get("Taukojen_valit")
+           avg.extend(seconds)
+           breakLengthDB.truncate()
+           breakLengthDB.insert({"Taukojen_valit" : avg})
+            
+#testing
+#addDatabase(testDate,testStudyTime,testBreakLength)
+#print(getBreakAvg())
+
