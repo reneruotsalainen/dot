e329cf7edc439b84b72d64c3a10458782d74083b
mikaelpo <47596730+mikaelpo@users.noreply.github.com>
2021-03-15 15:31:36 +0200

    Updates to main.js, index.html and db.json

--- DOT web/main.js
+++ DOT web/main.js
@@ -1,19 +1,76 @@
 
 $(document).ready(function(){
         $.getJSON("db.json", function(json) {
-        //console.log(json);
         Object.entries(json).forEach(([key, value]) => {
-            console.log("avain", key)
-            console.log("arvo ", value) 
-
+            let list = [];
             Object.entries(value).forEach(([key, value]) => {
-                console.log("avain", key)
-                console.log("arvo ", value) 
+                list.push(value);
 
                 $("#data").append(JSON.stringify(value, null, 4)) 
                 $("#data").append("<br>"); 
             }); 
-        });    
+            barChart(list); 
+            otherChart(list);
+        });  
+        
     });  
 });
 
+//Makes visualization of days and studytimes. Chart can be changed in type:"bar" section
+function barChart(studyTimes){
+
+    let days = [];
+    let times = [];
+    let backgroundColors = []
+    for (let i = 0; i < studyTimes.length; i++){
+        days.push(studyTimes[i].paiva.toString());
+        times.push(parseInt(studyTimes[i].Opiskeluaika)/60);
+        backgroundColors.push("rgba(255,0,0,0.5)");
+    }
+
+    let myChart = document.getElementById("barChart").getContext("2d");
+    let barChart = new Chart(myChart, {
+        type:"bar", //bar, horizontalBar, pie, line, doughnut, radar, polarArea
+        data:{
+            labels:days,
+            
+            datasets:[{
+                label:"Opiskeluaika",
+                data:times,  
+                backgroundColor:backgroundColors
+            }]
+        },
+        options:{
+            title:{
+                display:true,
+                text:"Opiskeluaikasi päivittäin"
+            },
+            legend:{
+                display:false,
+            },
+            scales:{
+                xAxes:[{
+                    display: true,
+                    scaleLabel:{
+                        display: true,
+                        labelString: 'Päivämäärä'
+                    }
+                }],
+                yAxes:[{
+                    display: true,
+                    ticks: {
+                        min: 0
+                    },
+                    scaleLabel:{
+                        display: true,
+                        labelString: 'Minuutit'
+                    }
+                }],
+            }
+        }
+    });
+}
+
+function otherChart(studyTimes){
+
+}
