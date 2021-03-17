33927a8723008e7b8b00fd46722f64b055ae9670
mikaelpo <47596730+mikaelpo@users.noreply.github.com>
2021-03-17 17:58:33 +0200

    Updates to main.js and index.html
    Added average break time and count to the website

--- DOT web/main.js
+++ DOT web/main.js
@@ -5,14 +5,27 @@ $(document).ready(function(){
             let list = [];
             Object.entries(value).forEach(([key, value]) => {
                 list.push(value);
-
+                console.log(list)
                 $("#data").append(JSON.stringify(value, null, 4)) 
                 $("#data").append("<br>"); 
             }); 
             barChart(list); 
-            otherChart(list);
+            avgBreakCount(list);
         });  
-        
+
+        $.getJSON("breakLengthDB.json", function(json) {
+            Object.entries(json).forEach(([key, value]) => {
+                let avgBreakLengths = [];
+                Object.entries(value).forEach(([key, value]) => {
+                    avgBreakLengths.push(value);
+                    //console.log(avgBreakLengths);
+                    $("#data").append(JSON.stringify(value, null, 4)) 
+                    $("#data").append("<br>"); 
+                }); 
+                //barChart(list); 
+                avgBreakTime(avgBreakLengths);
+            }); 
+        }); 
     });  
 });
 
@@ -24,7 +37,8 @@ function barChart(studyTimes){
     let backgroundColors = []
     for (let i = 0; i < studyTimes.length; i++){
         days.push(studyTimes[i].paiva.toString());
-        times.push(parseInt(studyTimes[i].Opiskeluaika)/60);
+        let tmp = (studyTimes[i].Opiskeluaika)/60
+        times.push(tmp.toFixed(3));
         backgroundColors.push("rgba(255,0,0,0.5)");
     }
 
@@ -71,6 +85,31 @@ function barChart(studyTimes){
     });
 }
 
-function otherChart(studyTimes){
+function avgBreakTime(studyTimes){
+    let breaks = [];
+    for (let i = 0; i < studyTimes.length; i++){
+        breaks.push(studyTimes[i].Taukojen_valit.toString());
+    }
+    let breaksString = breaks[0].split(",");
+   
+    const arrInteger = breaksString.map(x => Number.parseInt(x, 10));
+    let avg = 0;
+    for (let i = 0; i < arrInteger.length; i++){
+        avg += arrInteger[i];
+    }
+    avg = Math.round((avg/arrInteger.length)/60)
+    document.getElementById("avgBreakTime").innerHTML=avg.toString() + " minuuttia";
+}
 
+function avgBreakCount(studyTimes){
+    let breakCount = [];
+    for (let i = 0; i < studyTimes.length; i++){
+        breakCount.push(parseInt(studyTimes[i].Taukojen_lkm))
+    }
+    let avg = 0;
+    for (let i = 0; i < breakCount.length; i++){
+        avg += breakCount[i];
+    }
+    avg = avg/breakCount.length;
+    document.getElementById("avgBreakCount").innerHTML=avg.toString();
 }
